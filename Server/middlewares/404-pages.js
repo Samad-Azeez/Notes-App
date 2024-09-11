cd79e1942e52d013672df9a07ec2aa3e7bb6d39a
Samad-Azeez <azeezsamad2004@gmail.com>
2024-09-11 21:49:25 +0100

    Refactor 404-pages middleware to send JSON error message for non-existent routes

--- Server/middlewares/404-pages.js
+++ Server/middlewares/404-pages.js
@@ -1,4 +1,7 @@
 export const notFound = (req, res, next) => {
-    res.status(404).send('Page not found')
-    next()
+    // Set the response status to 404 for "Not Found"
+    res.status(404).send('Page not found'); // Send a simple message for non-existent routes
+
+    // Call the next middleware (if needed), though typically not necessary after sending a response
+    next();
 }

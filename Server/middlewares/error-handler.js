9216c77ae456df0a02cf2e75b1f212d4e0df83dd
Samad-Azeez <azeezsamad2004@gmail.com>
2024-09-11 21:48:00 +0100

    Refactor error-handler middleware to return error message in JSON format

--- Server/middlewares/error-handler.js
+++ Server/middlewares/error-handler.js
@@ -1,4 +1,7 @@
 export const errorHandler = (err, req, res, next) => {
-    res.status(404).json(err.message);
-    next()
+    // Set the response status code to 404 (as per the original code) and return the error message in JSON format
+    res.status(404).json({ message: err.message });
+
+    // Call the next middleware (though this isn't typically needed after sending a response)
+    next();
 }
